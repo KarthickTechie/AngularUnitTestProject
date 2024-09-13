05662a734915f950cfd11027e0699b4d6e3f2ee0
karthickTechie <reachkarthickprogramer@gmail.com>
2024-09-13 14:26:07 +0530

    feat : angular-google-charts libraray added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GoogleChartsModule , ChartType} from 'angular-google-charts';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [GoogleChartsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent {
-
+  chartType = ChartType.Bar
+  myData = [
+    ['London', 8136000],
+    ['New York', 8538000],
+    ['Paris', 2244000],
+    ['Berlin', 3470000],
+    ['Kairo', 19500000],
+  
+  ];
 }
